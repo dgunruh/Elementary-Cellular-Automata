7ebe59048a3b91aeb9874e7bebe2751be5258cc5
Davis <dgunruh@ucdavis.edu>
2020-04-21 08:47:30 -0700

    Updated file in vscode and incorporated issues

--- ECA_dgunruh.py
+++ ECA_dgunruh.py
@@ -2,76 +2,22 @@
 # coding: utf-8
 
 # # 3-state nearest neighbor cellular automata
-# ## First up will be the code without the use of functions or classes
+# ## First up will be the code with functions or classes
 
 import random
 from matplotlib import pyplot as plt
 
-rule_number = 7518
-length = 100
-time = 100
-
-# make the initial condition
-initial_condition = [random.randint(0, 2) for _ in range(length)]
-
-# create list of neighborhood tuples in lex. order
-neighborhoods = [(0, 0), (0, 1), (0, 2), (1, 0),
-                 (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
-
-# convert the rule number to ternary
-in_ternary = ''
-if rule_number == 0:
-    in_ternary = '0'
-else:
-    nums = []
-    n = rule_number
-    while n:
-        n, r = divmod(n, 3)
-        nums.append(str(r))
-    in_ternary = ''.join(reversed(nums))
-
-# reverse the rule number
-in_ternary = in_ternary[::-1]
-
-# pad the rule number with zeros
-ternary_length = len(in_ternary)
-if ternary_length != 9:
-    padding = 9 - ternary_length
-    in_ternary = in_ternary + '0' * padding
-
-lookup_table = dict(zip(neighborhoods, in_ternary))
-
-# initialize spacetime field and current configuration
-spacetime_field = [initial_condition]
-current_configuration = initial_condition.copy()
-
-# apply the lookup table to evolve the CA
-# for the given number of time steps
-for t in range(time):
-    new_configuration = []
-    for i in range(len(current_configuration)):
-
-        neighborhood = (current_configuration[(i - 1)],
-                        current_configuration[i])
-
-        new_configuration.append(int(lookup_table[neighborhood]))
-
-    current_configuration = new_configuration
-    spacetime_field.append(new_configuration)
-
-# plot the spacetime field diagram
-plt.figure(figsize=(12, 12))
-plt.imshow(spacetime_field, cmap=plt.cm.Greys, interpolation='nearest')
-plt.show()
-
-
-# ## Now we make the same code, but instead using functions and one class
-#
-# Let's start by making our core cellular automata class
-
 class ECA(object):
     '''
     3-site nearest neighbor cellular automata simulator
+
+    Example of running code:
+    length = 100
+    time_steps = 100
+    initial_condition = create_initial_condition(length)
+    rule_7518 = ECA(7518, initial_condition)
+    rule_7518.evolve(time_steps)
+    spacetime_diagram(rule_7518.spacetime)
     '''
 
     def __init__(self, rule_number, initial_condition):
@@ -140,6 +86,12 @@ class ECA(object):
         neighborhoods = [(0, 0), (0, 1), (0, 2), (1, 0),
                          (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
 
+        #Get the rule number
+        try:
+            rule_number = int(rule_number)
+        except ValueError:
+            raise ValueError("rule_number must be a non-negative integer")
+
         # convert the rule number to ternary
         in_ternary = ''
         if rule_number == 0:
@@ -163,12 +115,7 @@ class ECA(object):
 
         return dict(zip(neighborhoods, in_ternary))
 
-
-# Let's now make two additional functions.
-# One which will allow us to view what the cellular automata class creates,
-# and another which will create our initial condition
-
-def spacetime_diagram(spacetime_field, size=12, colors=plt.cm.Greys):
+def spacetime_diagram(spacetime_field, size=12, colors=plt.cm.get_cmap("Greys")):
     '''
     Produces a simple spacetime diagram image
     using matplotlib imshow with 'nearest' interpolation.
@@ -201,12 +148,4 @@ def create_initial_condition(length):
     if not isinstance(length, int) or length < 0:
         raise ValueError("input length must be a positive ingeter")
     return [random.randint(0, 2) for _ in range(length)]
-
-
-# Now let's put it all together
-length = 100
-time_steps = 100
-initial_condition = create_initial_condition(length)
-rule_7518 = ECA(7518, initial_condition)
-rule_7518.evolve(time_steps)
-spacetime_diagram(rule_7518.spacetime)
+    
